82b5dd5cda54df58b0f35e3cf86e2cea30dd33fb
nbirhanu <nbirhanu80@gmail.com>
2023-08-31 17:51:54 +0300

    display cities and country list

---
+++ src/components/Cityitem.jsx
@@ -0,0 +1,25 @@
+import styles from './Cityitem.module.css'
+import { Link } from 'react-router-dom'
+const formatDate = date =>
+	new Intl.DateTimeFormat('en', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+		weekday: 'long',
+	}).format(new Date(date))
+function Cityitem({ city }) {
+	console.log(city)
+	const { cityName, emoji, date, id, position } = city
+	return (
+		<li>
+			<Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+				<span className={styles.emoji}>{emoji}</span>
+				<h2 className={styles.name}>{cityName}</h2>
+				<time className={styles.date}>{formatDate(date)}</time>
+				<button className={styles.deleteBtn}>&times;</button>
+			</Link>
+		</li>
+	)
+}
+
+export default Cityitem
